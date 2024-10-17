029ac7159576be2d67e5653958cf4950fa3cf919
kersor <vasilev.rustik.03@mail.ru>
2024-10-17 19:52:56 +0300

    ограничил кол-во выдаваемых категорий через обрезание массива

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,31 +14,10 @@ import { useRef, useState } from "react";
 
 
 export default function Home() {
-  const [activeCategory, setActiveCategory] = useState<number>(0)
-
   return (
       <Container className="pt-[90px]" >
-        {/* <TopBar /> */}
-        <Flex className="justify-between">
-          <Flex 
-            className="gap-[5px] items-center">
-            {
-              FILTER_CATEGORY.data.map((item, index) => (
-                index < 5 && 
-                <Button 
-                    key={item.value}
-                    onClick={() => setActiveCategory(index)} 
-                    variant="outline" 
-                    size="smal" 
-                    className={cn("rounded-[50px] text-[12px] ", activeCategory === index ? "bg-[#000] text-[#FFF]" : "")}
-                  >
-                  {item.title}
-                </Button>
-              ))
-            }
-          </Flex>
-          <Link className="relative after:absolute after:bottom-[0] after:left-[50%] after:translate-x-[-50%] after:w-[90%] after:h-[1px] after:bg-none hover:after:bg-[#000] " href="/shop">Смотреть все</Link>
-        </Flex>
+        <TopBar />
+        
       </Container>
   );
 }
--- frontend/components/shared/Top-bar.tsx
+++ frontend/components/shared/Top-bar.tsx
@@ -3,6 +3,9 @@ import { Flex } from "./Flex"
 
 import { SelectCustom } from "../ui/select-custom"
 import { FILTER_BRANDS, FILTER_CATEGORY, FILTER_COLORS } from "@/config/filters.config"
+import { useState } from "react"
+import { Button } from "../ui/button"
+import Link from "next/link"
   
 
 interface ITopBar {
@@ -12,20 +15,28 @@ interface ITopBar {
 export function TopBar ({
     className
 }: ITopBar) {
+    const [activeCategory, setActiveCategory] = useState<number>(0)
+    const data = FILTER_CATEGORY.data.slice(0, 5)
+    
     return (
-        <Flex className={cn("mt-[20px] gap-[10px]", className)}> 
-            <SelectCustom 
-                data={FILTER_CATEGORY.data}
-                placeholder={FILTER_CATEGORY.placeholder}
-            />
-            <SelectCustom 
-                data={FILTER_BRANDS.data}
-                placeholder={FILTER_BRANDS.placeholder}
-            />
-            <SelectCustom 
-                data={FILTER_COLORS.data}
-                placeholder={FILTER_COLORS.placeholder}
-            />
+        <Flex className={cn("mt-[20px] gap-[10px] justify-between", className)}> 
+          <Flex 
+            className="gap-[5px] items-center">
+            {
+              data.map((item, index) => (
+                <Button
+                    key={item.value}
+                    onClick={() => setActiveCategory(index)} 
+                    variant="outline" 
+                    size="smal" 
+                    className={cn("rounded-[50px] text-[12px] ", activeCategory === index ? "bg-[#000] text-[#FFF]" : "")}
+                  >
+                  {item.title}
+                </Button>
+              ))
+            }
+          </Flex>
+          <Link className="relative after:absolute after:bottom-[0] after:left-[50%] after:translate-x-[-50%] after:w-[90%] after:h-[1px] after:bg-none hover:after:bg-[#000]" href="/shop">Смотреть все</Link>
         </Flex>
     )
 }
