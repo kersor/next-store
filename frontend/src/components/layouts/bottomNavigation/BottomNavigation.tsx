e3cc1379ab3d5af9efa5b32005236336c8f1efa5
svolochyo <127143304+kersor@users.noreply.github.com>
2024-12-17 17:46:08 +0300

    Add: media на удаление при ширине

--- frontend/src/components/layouts/bottomNavigation/BottomNavigation.tsx
+++ frontend/src/components/layouts/bottomNavigation/BottomNavigation.tsx
@@ -10,7 +10,7 @@ export const BottomNavigation = ({
     className
 }: Props) => {
     return (
-        <div style={{boxShadow: "0 -2px 8px #00000026"}} className={clsx("fixed w-full h-[70px] bottom-0 left-0 border-t shadow-lg shadow-black-500/50", className)}>
+        <div style={{boxShadow: "0 -2px 8px #00000026"}} className={clsx("fixed w-full h-[70px] bottom-0 left-0 border-t shadow-lg shadow-black-500/50 min-[800px]:hidden", className)}>
             <Container className="flex items-center justify-around h-full text-[14px]">
                 {NAVIGATION_ITEM.map(item => (
                     <div className="flex flex-col items-center">
