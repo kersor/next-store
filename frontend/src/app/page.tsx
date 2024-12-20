38091889e32c59333312361a70757d43a7981f05
kersor <vasilev.rustik.03@mail.ru>
2024-12-20 23:53:15 +0300

    Update banner

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <Container>
-      <div className="w-full h-[400px] relative flex items-center">
+      <div className="w-full h-[300px] relative flex items-center rounded-[10px]">
         <div className="z-[1] ml-[100px]">
-          <div className="text-[45px] uppercase max-w-[600px] font-StolzlMedium leading-[45px]">Хватай до 50% на некоторые наушники</div>
-          <Link className="mt-[30px] px-10 py-3 bg-[#37434f] rounded-full text-[#FFF] text-[14px]" href="/">Купить</Link>
+          <div className="text-[45px] uppercase max-w-[600px] font-StolzlMedium leading-[45px] pb-[20px] text-[#000]">Хватай до 50% на некоторые наушники</div>
+          <Link className="px-10 py-3 bg-[#b3be62] rounded-full text-[#FFF] text-[14px]" href="/">Купить</Link>
         </div>
-        <img className="absolute top-0 left-0 w-full h-full object-cover -z-[1000]" src="/photos/photo_banner.png" alt="" />
+        <img className="rounded-[10px] absolute top-0 left-0 w-full h-full object-cover -z-[1000]" src="/photos/photo_banner.png" alt="" />
       </div>
     </Container>
   );
